c3efe1963429dee29cb75a648962fb5ad3fced44
lemonmagic <1073874514@qq.com>
2021-10-01 22:35:09 +0800

    Site updated: 2021-10-01 22:34:57

--- HTML/Lab01/js/ch01/tri_squ1.js
+++ HTML/Lab01/js/ch01/tri_squ1.js
@@ -12,8 +12,6 @@ window.onload = function init() {
 	gl.viewport(0, 0, canvas.width, canvas.height);
 	//设置背景色清空canvas对象（R,G,B,A）
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
-	//清空颜色缓冲区
-	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	// Load shaders and initialize attribute buffers
 	var program = initShaders(gl, "vertex-shader", "fragment-shader");
@@ -29,20 +27,46 @@ window.onload = function init() {
      1.0,  0.5,
      1.0, -0.5,
 	];
+	var colors = [
+		1.0, 0.0, 0.0, 1.0,
+		0.0, 1.0, 0.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+		0.0, 0.0, 1.0, 1.0,
+	]
 	// 创建缓冲区对象
 	var bufferId = gl.createBuffer();
   // 绑定缓冲区对象
 	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
   // 对缓冲区写入数据
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	var FSIZE = vertices.BYTES_PER_ELEMENT;
 	// 返回了给定WebGLProgram对象中某属性的下标指向位置
 	var vPosition = gl.getAttribLocation(program, "vPosition");
   // 将缓冲区对象分配给一个attribute变量
-	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, FSIZE*5, 0);
+	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
 	// 开启attribute变量
 	gl.enableVertexAttribArray(vPosition);
-  // 绘制
+
+	// 创建缓冲区对象
+	var bufferId2 = gl.createBuffer();
+  // 绑定缓冲区对象
+	gl.bindBuffer(gl.ARRAY_BUFFER, bufferId2);
+  // 对缓冲区写入数据
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+	// 返回了给定WebGLProgram对象中某属性的下标指向位置
+	var vColor = gl.getAttribLocation(program, "vColor");
+  // 将缓冲区对象分配给一个attribute变量
+	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+	// 开启attribute变量
+	gl.enableVertexAttribArray(vColor);
+
+	render();
+}
+
+function render(){
+	gl.clear( gl.COLOR_BUFFER_BIT );
 	gl.drawArrays( gl.TRIANGLES, 0, 3 );
-  gl.drawArrays( gl.TRIANGLE_FAN, 3, 4);
+	gl.drawArrays( gl.TRIANGLE_FAN, 3, 4);
 }
